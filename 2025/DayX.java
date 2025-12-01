098d02d4854b4043f14404c891e9e2811b236732
chikenboi2 <113371469+chikenboi2@users.noreply.github.com>
2025-12-01 01:10:02 -0500

    Add files via upload

---
+++ 2025/DayX.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+// Template for Day X
+public class DayX {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(new FileReader("./DayX.txt"));
+        
+        part1();
+        
+        in = new Scanner(new FileReader("./DayX.txt"));
+        part2();
+
+        in.close();
+    }
+
+    public static void part1() {
+
+    }
+
+    public static void part2() {
+
+    }
+}
