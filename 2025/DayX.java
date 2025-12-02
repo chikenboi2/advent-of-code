047b360f87dee071116896b603548a3e17fceac7
chikenboi2 <113371469+chikenboi2@users.noreply.github.com>
2025-12-02 01:20:29 -0500

    update

--- 2025/DayX.java
+++ 2025/DayX.java
@@ -1,23 +1,23 @@
 import java.util.*;
 import java.io.*;
 // Template for Day X
+
 public class DayX {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(new FileReader("./DayX.txt"));
-        
-        part1();
+        part1(in);
         
         in = new Scanner(new FileReader("./DayX.txt"));
-        part2();
+        part2(in);
 
         in.close();
     }
 
-    public static void part1() {
+    public static void part1(Scanner in) {
 
     }
 
-    public static void part2() {
+    public static void part2(Scanner in) {
 
     }
 }
